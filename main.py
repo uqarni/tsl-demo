07f73b1ede393bfa4034ca4dfd4db02dd54285db
uqarni <108438834+uqarni@users.noreply.github.com>
2023-10-12 03:05:06 +0000

    works

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from functions import ideator, secret_message
+from functions import ideator
 import json
 import os
 import sys
@@ -11,32 +11,27 @@ urL: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 
 supabase: Client = create_client(urL, key)
-data, count = supabase.table("bots_dev").select("*").eq("id", "emily").execute()
+data, count = supabase.table("bots_dev").select("*").eq("id", "sam").execute()
 bot_info = data[1][0]
 
 
 
 def main():
     # Create a title for the chat interface
-    st.title("FullHarvest Bot (named Emily)")
+    st.title("TSL Bot (named Sam)")
     st.write("To test, first select some fields then click the button below.")
   
 
-    name = 'Emily'
-    booking_link = 'fullharvestbooking.com'
+    name = 'Sam'
+    booking_link = 'tslbooking.com'
     lead_first_name = st.text_input('Lead First Name', value = 'John')
-    lead_type = st.selectbox("lead type",["supplier", "provider"])
-    produce_type = st.selectbox("produce type", ["tomatoes", ]
     system_prompt = bot_info['system_prompt']
     initial_text = bot_info['initial_text']
     
 
     
     if st.button('Click to Start or Restart'):
-        #variable inputs
-
-
-        system_prompt.format(lead_first_name=lead_first_name, booking_link = booking_link, name=name)
+        system_prompt = system_prompt.format(lead_first_name=lead_first_name, booking_link = booking_link, name=name)
 
         initial_text = initial_text.format(lead_first_name = lead_first_name, name=name)
 
@@ -97,69 +92,6 @@ def main():
             if 'This is a secret internal thought' not in str(message):
                 string = string + message["role"] + ": " + message["content"] + "\n\n"
         st.write(string)
-
-    if day.activation_date == 'no':
-        if st.button("Toggle Activation"):
-            if day.activation_date == "no":
-                day.activation_date = "yes" 
-            #extract messages out to list
-            messages = []
-
-            with open('database.jsonl', 'r') as f:
-                for line in f:
-                    json_obj = json.loads(line)
-                    messages.append(json_obj)
-
-            # Display the response in the chat interface
-            string = ""
-
-            for message in messages[1:]:
-                if 'secret internal thought' not in str(message):
-                    string = string + message["role"] + ": " + message["content"] + "\n\n"
-            st.write(string)
-
-    
-    if st.button("Increment Day"):
-        increment_variable(day)
-        
-        if int(day.my_var) <= 7:
-            
-            newline = secret_message(day.my_var, day.activation_date)
-            with open('database.jsonl', 'a') as f:
-                f.write(json.dumps(newline) + '\n')
-        
-            # Your existing code for reading the database, generating responses, and updating the database can remain here
-            # extract messages out to list
-            messages = []
-    
-            with open('database.jsonl', 'r') as f:
-                for line in f:
-                    json_obj = json.loads(line)
-                    messages.append(json_obj)
-    
-            #generate OpenAI response
-            messages, count = ideator(messages)
-    
-            #append to database
-            with open('database.jsonl', 'a') as f:
-                    for i in range(count):
-                        f.write(json.dumps(messages[-count + i]) + '\n')
-    
-    
-    
-            # Display the response in the chat interface
-            string = ""
-    
-            for message in messages[1:]:
-                if "secret internal thought"  not in str(message):
-                    string = string + message["role"] + ": " + message["content"] + "\n\n"
-            st.write(string)
-        else:
-            st.write('Trial ended. Bot will not send messages. Please reset to start again.')
-
-    # At the bottom of your Streamlit layout, you can show the current week
-    st.write(f"*Currently in Day:* {day.my_var}")
-    st.write(f"*is user activated?* {day.activation_date}")
         
 
 if __name__ == '__main__':
